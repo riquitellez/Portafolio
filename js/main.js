cd6fdbe5faa063988db9b345811becaeb8e5c40d
riquitellez <54512082+riquitellez@users.noreply.github.com>
2021-01-22 23:02:19 +0100

    Week 3 assignment

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    },
+    {
+        label: "Week2 notes",
+        url: "week1/index.html"
+    }
+  ]
+
+  for(var i = 1; i<links.)
+
